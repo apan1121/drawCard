0dce28e9209e3675047e3a1463734029608ca568
byron <byron@pressplay.cc>
2024-01-07 17:18:56 +0800

    [deploy]

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
     
-        $ver = "e39817c";
+        $ver = "17f0136";
         define('WEB_VER', $ver);
-        $ver_time = "1704618326";
+        $ver_time = "1704618881";
         define('WEB_VER_TIME', $ver_time);
     
