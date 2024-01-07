4988a10f1d9a4ccd1fb3629e578b9a47561dc16d
byron <byron@pressplay.cc>
2024-01-07 17:13:16 +0800

    [deploy]

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
     
-        $ver = "96ee52e";
+        $ver = "e39817c";
         define('WEB_VER', $ver);
-        $ver_time = "1704616264";
+        $ver_time = "1704618326";
         define('WEB_VER_TIME', $ver_time);
     
